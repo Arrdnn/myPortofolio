63700fec28d2771027455475ab9d3b8f086dac12
Arrdnn <ardnrzki@gmail.com>
2023-06-07 11:58:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const option = document.querySelector('.option ul');
+
+menuToggle.addEventListener('click', function () {
+    option.classList.toggle('geser');
+});
+
+var btnContainer = document.getElementById("nav");
+var btns = btnContainer.getElementsByClassName("btn");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
